9eea463743b8786b92b8b39b7be66dfc7bcc0593
unknown <momohoho2224@gmail.com>
2024-04-18 20:07:22 +0200

    create trips and destinations migrations

---
+++ BackEnd/database/migrations/2024_04_18_175352_create_trips_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    // trips (#id,bus_id, from , to, date, price)
+    public function up(): void
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            // $table->foreignId("bus_id")->constrained();
+            $table->unsignedBigInteger('bus_id');
+            $table->foreign('bus_id')->references('id')->on('buses')->onDelete('cascade');
+            $table->unsignedBigInteger('from');
+            $table->foreign('from')->references('id')->on('destinations')->onDelete('cascade');
+            $table->unsignedBigInteger('to');
+            $table->foreign('to')->references('id')->on('destinations')->onDelete('cascade');
+            $table->unsignedBigInteger("price");
+            $table->date("date");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trips');
+    }
+};
