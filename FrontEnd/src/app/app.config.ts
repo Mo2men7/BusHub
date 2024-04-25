ae62cb80a3da7d053e5691d10c6f9230a6bfd533
Amira-Elsayed-Abdelaziz <amira.elsayed.abdelaziz@gmail.com>
2024-04-25 18:54:28 +0200

    change sidebar in admin

--- FrontEnd/src/app/app.config.ts
+++ FrontEnd/src/app/app.config.ts
@@ -2,12 +2,9 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter ,withComponentInputBinding, withViewTransitions} from '@angular/router';
 
 import { routes } from './app.routes';
-// !
-import { HttpClientModule, provideHttpClient } from '@angular/common/http';
-import { importProvidersFrom } from '@angular/core';
-// !
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  // providers: [provideRouter(routes)]
-  providers: [importProvidersFrom(HttpClientModule), provideRouter(routes,withComponentInputBinding(), withViewTransitions()),provideHttpClient()],
+  providers: [provideRouter(routes),provideHttpClient()]
+  // providers: [importProvidersFrom(HttpClientModule), provideRouter(routes,withComponentInputBinding(), withViewTransitions()),provideHttpClient()],
 };
