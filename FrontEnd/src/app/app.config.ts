06a84c4be9e5ccbfc5a5bbe9295e936f58e068d3
Mo2men7 <momen.helmy.m@gmail.com>
2024-04-27 23:07:27 +0300

    apply some changes

--- FrontEnd/src/app/app.config.ts
+++ FrontEnd/src/app/app.config.ts
@@ -6,6 +6,6 @@ import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes),provideHttpClient(), provideAnimationsAsync()]
+  providers: [provideRouter(routes),provideHttpClient(), provideAnimationsAsync(), provideAnimationsAsync()]
   // providers: [importProvidersFrom(HttpClientModule), provideRouter(routes,withComponentInputBinding(), withViewTransitions()),provideHttpClient()],
 };
