7dd81677a7f667817dfd8ac8b627f9e0d926a822
Amira-Elsayed-Abdelaziz <amira.elsayed.abdelaziz@gmail.com>
2024-04-27 21:35:30 +0300

    changing destination ui

--- FrontEnd/src/app/admin/destinations/destinations.component.ts
+++ FrontEnd/src/app/admin/destinations/destinations.component.ts
@@ -11,7 +11,7 @@ import { FormsModule } from '@angular/forms';
 })
 export class DestinationsComponent {
   destinations: any;
-  newDist:any;
+  newDist: any;
   constructor(private destinationService: DestinationService) {}
 
   ngOnInit() {
@@ -21,21 +21,47 @@ export class DestinationsComponent {
     );
   }
   sendDestination(form: any) {
-    this.newDist={
-      'name':form.form.value.name,
-      'info':form.form.value.info,
-      'flag':form.form.value.flag,
-      'pic':form.form.value.pic,
-      'map':form.form.value.map,
-    }
-    
-    
-    this.destinationService.addDestinations(this.newDist).subscribe((res)=>this.ngOnInit(),
+    this.newDist = {
+      name: form.form.value.name,
+      info: form.form.value.info,
+      flag: form.form.value.flag,
+      pic: form.form.value.pic,
+      map: form.form.value.map,
+    };
+
+    this.destinationService.addDestinations(this.newDist).subscribe(
+      (res) => this.ngOnInit(),
       (error) => console.log(error)
     );
     // console.log(form);
   }
-  // display() {
-  //   this.
-  //   );
+  showMap(id: any) {
+    const getFrame = document.getElementsByTagName('iframe')[0];
+    const getDivFrame = document.getElementById('iframeDiv');
+    getDivFrame?.classList.remove('fadein');
+    getDivFrame?.classList.add('elementToFadeout');
+    setTimeout(function(){getDivFrame?.classList.remove("elementToFadeout");}, 5000);
+    getDivFrame?.classList.add('fadeout');
+    const map = this.destinations[id].map;
+    getFrame.src = map;
+    getDivFrame?.classList.add('elementToFadeIn');
+
+    setTimeout(function(){getDivFrame?.classList.remove("elementToFadeIn");}, 5000);
+    getDivFrame?.classList.remove('fadeout');
+    getDivFrame?.classList.add('fadein');
+    
+    
+
+    // getFrame.classList.add('fade-in');
+    console.log(id);
+    console.log(this.destinations);
+  }
+//   var div = document.querySelector(".fade");
+
+// btn.addEventListener("click", function(){
+//   div.classList.add("elementToFadeInAndOut");
+//   // Wait until the animation is over and then remove the class, so that
+//   // the next click can re-add it.
+//   setTimeout(function(){div.classList.remove("elementToFadeInAndOut");}, 4000);
+// });
 }
