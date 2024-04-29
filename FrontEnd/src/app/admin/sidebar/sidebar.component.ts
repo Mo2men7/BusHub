64809fbf1c424acd0cdcaea1be2dfd324620d758
Amira-Elsayed-Abdelaziz <amira.elsayed.abdelaziz@gmail.com>
2024-04-29 18:06:33 +0300

    changing sidebar style and color of map

--- FrontEnd/src/app/admin/sidebar/sidebar.component.ts
+++ FrontEnd/src/app/admin/sidebar/sidebar.component.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
 import { DestinationsComponent } from '../destinations/destinations.component';
 import { BusesComponent } from '../buses/buses.component';
-import { RouterLink,RouterLinkActive } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [DestinationsComponent,BusesComponent,RouterLink,RouterLinkActive],
+  imports: [
+    DestinationsComponent,
+    BusesComponent,
+    RouterLink,
+    RouterLinkActive,
+  ],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrl: './sidebar.component.css',
 })
 export class SidebarComponent {
-
+  showIcons() {
+    const showSmall = document.getElementsByClassName('linkInSmallbar');
+    const showBig = document.getElementsByClassName('linkInBigbar');
+    // for (var i = 0; i < showBig.length; i++) 
+    //   {
+    //     showBig[i].classList.toggle('hideIcons');
+    //   }
+    for (var i = 0; i < showSmall.length; i++) 
+      {
+        showSmall[i].classList.toggle('hideIcons');
+      }
+    console.log();
+  }
 }
