bdf93afdb477954364c689e1c7378f18f66d4516
mahmoudtawfek22 <momohoho2224@gmail.com>
2024-04-30 21:59:38 +0300

    edit users

--- BackEnd/database/migrations/2014_10_12_000000_create_users_table.php
+++ BackEnd/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,9 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->enum('role', ['user', 'admin'])->default('user');
+            $table->date('birth_date')->nullable();
+            $table->enum('gender', ['Female', 'Male'])->nullable();
+            $table->string('city')->nullable();
+            $table->string('phone', 12)->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
