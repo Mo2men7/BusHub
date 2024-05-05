e5be32a2161e346839e3177ccee5f0900a393a87
Amira-Elsayed-Abdelaziz <amira.elsayed.abdelaziz@gmail.com>
2024-05-05 20:36:49 +0300

    adding uploading files in destinations

--- BackEnd/app/Http/Controllers/admin/DestinationAdminController.php
+++ BackEnd/app/Http/Controllers/admin/DestinationAdminController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\admin;
 use App\Models\admin\Destination;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
 
 class DestinationAdminController extends Controller
 {
@@ -18,7 +20,24 @@ class DestinationAdminController extends Controller
      */
     public function store(Request $request)
     {
-        return Destination::create($request->all());
+        $req = $request->all();
+        // Store the file in storage\app\public folder
+        $imageName = $request->file('pic')->hashName();
+        $imageEx = $request->file('pic')->getClientOriginalExtension();
+        $fileName = $imageName;
+        $destinationPath = config('app.picDestinationPath') . "/pic/" . $fileName;
+        Storage::disk('local')->put($destinationPath, file_get_contents($request->file('pic')->getRealPath()));
+        $req["pic"] = $destinationPath;
+        
+        //!  
+        $flagName = $request->file('flag')->hashName();
+        $flagEx = $request->file('flag')->getClientOriginalExtension();
+        $fileName = $flagName;
+        $destinationPath = config('app.flagDestinationPath') . "/flag/" . $fileName;
+        Storage::disk('local')->put($destinationPath, file_get_contents($request->file('flag')->getRealPath()));
+        $req["flag"] = $destinationPath;
+
+        return   Destination::create($req);
     }
 
     /**
