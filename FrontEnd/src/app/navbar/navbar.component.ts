78cb92577dbfe1538c8ecee49d3f4e3a35c4572c
Mo2men7 <momen.helmy.m@gmail.com>
2024-05-07 14:50:13 +0300

    replace login button with profile user name if token exist

--- FrontEnd/src/app/navbar/navbar.component.ts
+++ FrontEnd/src/app/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { UserService } from '../services/user.service';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-navbar',
@@ -9,5 +11,14 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+  constructor(private userservice: UserService, private cookie: CookieService){}
+  userData: any;
+  token = this.cookie.get("token");
+  ngOnInit(){
+    this.userservice.userProfile(this.token).subscribe(
+      res => {
+        this.userData = res;
+      }
+    )
+  }
 }
