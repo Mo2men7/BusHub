356345b059d3b8ac3a5a63fa89cf3deca9679400
Amira-Elsayed-Abdelaziz <amira.elsayed.abdelaziz@gmail.com>
2024-05-11 16:41:23 +0300

    editing trips in admin

--- FrontEnd/src/app/admin/trips/trips.component.ts
+++ FrontEnd/src/app/admin/trips/trips.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TripService } from '../services/tripService/trip.service';
+import { BusService } from '../services/busService/bus.service';
 @Component({
   selector: 'app-trips',
   standalone: true,
@@ -9,7 +10,9 @@ import { TripService } from '../services/tripService/trip.service';
 })
 export class TripsComponent {
   trips: any;
-  constructor(private tripService: TripService) {}
+  buses:any;
+  countBuses:any;
+  constructor(private tripService: TripService,private busService: BusService) {}
   ngOnInit() {
     this.tripService.listTrips().subscribe(
       (res: any) => {
@@ -18,6 +21,35 @@ export class TripsComponent {
       },
       (error) => console.log(error)
     );
+    this.busService.listBuses().subscribe(
+      (res: any) => {
+        this.buses = res;
+        // console.log(res.length);
+        this.countBuses=res.length;
+      },
+      (error) => console.log(error)
+    );
+  }
+  expand(id: any) {
+    for (var i = 0; i < this.trips.length; i++) {
+      const id2 = 'Exp' + this.trips[i].id;
+      const div = document.getElementById(id2);
+      div?.classList.remove('order-0');
+      div?.classList.remove('order-'+(i+1));
+      if (id2 == id) continue;
+      div?.classList.add('order-'+(i+1));
+      div?.classList.add('cardCollapse');
+      div?.classList.remove('cardExpand');
+    }
+    const div2 = document.getElementById(id);
+    if (div2?.classList.contains('cardExpand')) {
+      div2?.classList.remove('cardExpand');
+      div2?.classList.add('cardCollapse');
+    } else {
+      div2?.classList.add('cardExpand');
+      div2?.classList.add('order-0');
+      div2?.classList.remove('cardCollapse');
+  
+    }
   }
 }
-
