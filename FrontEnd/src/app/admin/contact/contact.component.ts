ba9edeac093335b04cc98284f86eab97dd422859
Mo2men7 <momen.helmy.m@gmail.com>
2024-05-12 19:51:52 +0300

    Apply delete method for contact us page in admin

--- FrontEnd/src/app/admin/contact/contact.component.ts
+++ FrontEnd/src/app/admin/contact/contact.component.ts
@@ -17,8 +17,23 @@ export class ContactComponent {
       console.log(res);
       this.ContactUsData=res;
     })
+    console.log(this.ContactUsData);
   }
   navigateToSingleContact(id:any){
     this.router.navigate([`admin/single-contact-us/${id}`])
+  }
+  deleteItem(id: any) {
+    this.http.delete(`http://127.0.0.1:8000/api/contactus/${id}`).subscribe(
+      () => {
+        console.log('Item deleted successfully');
+        this.removeDeletedItemFromUI(id);
+      },
+      (error) => {
+        console.error('Error deleting item:', error);
+      }
+    );
+  }
+  removeDeletedItemFromUI(deletedItemId: any) {
+    this.ContactUsData = this.ContactUsData.filter((data: { id: any; }) => data.id !== deletedItemId);
   }
 }
